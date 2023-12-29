1442773b819ce353f8edad2f18934a5994c7a256
Samir70 <sbetmouni@talktalk.net>
2023-12-29 12:29:14 +0000

    set up server to serve react app

--- server/server.js
+++ server/server.js
@@ -9,10 +9,15 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
+app.use(express.static(path.join(__dirname, "../build")))
 
 app.use("/api/enquiries", enquiriesController)
 
 const port = process.env.PORT || 4005
 app.listen(port, () => {
   console.log(`Server is running on port ${port}`)
+})
+
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname, "../build/index.html"))
 })
